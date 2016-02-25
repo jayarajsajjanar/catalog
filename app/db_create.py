a54780a5e255cc59b44089fe51a67dc303d6f958
jayarajsajjanar <jayarajsajjanar2010@gmail.com>
2016-02-25 20:41:34 +0530

    db_create.py in app folder creates tables, adds, instantiates

---
+++ app/db_create.py
@@ -0,0 +1,18 @@
+from models import *
+
+db.create_all()
+c1=Cat("hockey")
+c1.name
+i1=Items("Hockey Stick","Its made of wood, curved at the end. Rubber grip for better control",c1)
+i2=Items("Hockey Ball","Usually white in color. Very hard.",c1)
+c2=Cat("Football")
+i3=Items("Ball","1 Feet diameter. Made of synthetic rubber.",c2)
+i4=Items("Shoes","Made of rubber.",c2)
+db.session.add(c1,c2)
+db.session.add(i1,i2)
+db.session.add(i3,i4)
+db.session.commit()
+
+# items_py=[()]
+# for i in Items.query.all():
+# 	items_py.append((i.id,i.Naming,i.Description))
--- app/views.py
+++ app/views.py
@@ -1,11 +1,32 @@
 from app import app
-from models import *
+
+
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
 
+#Could not place this import statement at the top. 404 error occuring.
+from models import *
 
 @app.route('/categories_all')
 def Categories():
-	return "All Categories"
+	# db.create_all()
+	# c1=Cat("hockey")
+	# c1.name
+	# i1=Items("Hockey Stick","Its made of wood, curved at the end. Rubber grip for better control",c1)
+	# i2=Items("Hockey Ball","Usually white in color. Very hard.",c1)
+	# c2=Cat("Football")
+	# i3=Items("Ball","1 Feet diameter. Made of synthetic rubber.",c2)
+	# i4=Items("Shoes","Made of rubber.",c2)
+	# db.session.add(c1,c2)
+	# db.session.add(i1,i2)
+	# db.session.add(i3,i4)
+	# db.session.commit()
+
+	# items_py=[()]
+	# for i in Items.query.all():
+	# 	items_py.append((i.id,i.Naming,i.Description))
+
+	return "Categories!!"
+
