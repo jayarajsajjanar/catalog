f18cacdd28c92a0e0d78726d6b36e801669d7bc7
jayarajsajjanar <jayarajsajjanar2010@gmail.com>
2016-03-03 20:09:10 +0530

    orm working fine

--- app/views.py
+++ app/views.py
@@ -1,32 +1,20 @@
+from flask import render_template
 from app import app
-
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Miguel'}  # fake user
+    return render_template('index.html',title='Home',user=user)
 
 #Could not place this import statement at the top. 404 error occuring.
-from models import *
+from models import Items
+from models import Cat
 
 @app.route('/categories_all')
 def Categories():
-	# db.create_all()
-	# c1=Cat("hockey")
-	# c1.name
-	# i1=Items("Hockey Stick","Its made of wood, curved at the end. Rubber grip for better control",c1)
-	# i2=Items("Hockey Ball","Usually white in color. Very hard.",c1)
-	# c2=Cat("Football")
-	# i3=Items("Ball","1 Feet diameter. Made of synthetic rubber.",c2)
-	# i4=Items("Shoes","Made of rubber.",c2)
-	# db.session.add(c1,c2)
-	# db.session.add(i1,i2)
-	# db.session.add(i3,i4)
-	# db.session.commit()
-
-	# items_py=[()]
-	# for i in Items.query.all():
-	# 	items_py.append((i.id,i.Naming,i.Description))
-
-	return "Categories!!"
+	items_py=[()]
+	for i in Items.query.all():
+		items_py.append((i.id,i.Naming,i.Description))
+	return render_template('categories_all.html',title='Categories_all',Items=items_py)
 
